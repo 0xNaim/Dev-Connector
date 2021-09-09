2c1315f18cb2a91a868ca28f00e1708a59b99e51
0xNaim <naimislam3545@gmail.com>
2021-09-09 23:04:54 +0600

    prepared for deploy

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
